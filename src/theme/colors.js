3deacd8675b6490184eea364f29149093465eacf
Peter Pan <peter.pan@shoplineapp.com>
2021-02-07 22:20:39 +0800

    feat(theme): create theme

---
+++ src/theme/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  BLACK_DEFAULT: '#231a3c',
+  BLACK_100: '#f2f1f5',
+  BLACK_200: '#4e4c73',
+  WHITE_DEFAULT: '#e6e5e8',
+  PURPLE_DEFAULT: '#ab67ec',
+};
+
+export default colors;

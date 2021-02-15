bde96146581d413d2d6d52ceb828ba89acdf2524
Peter Pan <peter.pan@shoplineapp.com>
2021-02-15 13:14:19 +0800

    feat(context): create sidebar context

---
+++ src/context/sidebar.jsx
@@ -0,0 +1,32 @@
+import React, {
+  createContext, useState, useCallback, useMemo, useEffect,
+} from 'react';
+
+const SidebarContext = createContext({
+  isOpen: true,
+  toggleOpen: () => {},
+});
+
+const { Provider } = SidebarContext;
+
+const SidebarProvider = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(() => window.innerWidth > 768);
+
+  useEffect(() => {
+    const handleResize = (e) => {
+      const { innerWidth } = e.target;
+      if (innerWidth < 768) setIsOpen(false);
+      else setIsOpen(true);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const toggleOpen = useCallback(() => setIsOpen((prev) => !prev), []);
+  const contextValue = useMemo(() => ({
+    isOpen, toggleOpen,
+  }), [isOpen, toggleOpen]);
+
+  return (<Provider value={contextValue}>{children}</Provider>);
+};
+export { SidebarContext, SidebarProvider };

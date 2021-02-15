728c2a938224047cf4d39d017f1ddb36b6718d34
Peter Pan <peter.pan@shoplineapp.com>
2021-02-15 13:18:31 +0800

    feat(App): add Theme / Sidebar / Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
+import { SidebarProvider } from './context/sidebar';
+import theme from './theme';
 import Pages from './pages';
 
 function App() {
   return (
-    <div>
-      <Pages />
-    </div>
+    <ThemeProvider theme={theme}>
+      <SidebarProvider>
+        <Router>
+          <Pages />
+        </Router>
+      </SidebarProvider>
+    </ThemeProvider>
   );
 }
 
